fef81e1e6558663284af93c16253ea453af9ee57
navent <jmarshall@navent>
2018-03-16 12:03:53 -0300

    New endpoint to upload image

--- run_keras_server.py
+++ run_keras_server.py
@@ -11,7 +11,6 @@ from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
 from PIL import Image
-from flask_cache import Cache
 import numpy as np
 import requests
 import flask
@@ -30,14 +29,15 @@ app = flask.Flask(__name__)
 # set the project root directory as the static folder, you can set others.
 #app = Flask(__name__, static_url_path='')
 model = None
-cache = Cache(app)
 
 def load_model():
 	# load the pre-trained Keras model (here we are using a model
 	# pre-trained on ImageNet and provided by Keras, but you can
 	# substitute in your own networks just as easily)
 	global model
+	print("LLEHUE")
 	model = ResNet50(weights="imagenet")
+	print("ME ESTOY POR IR")
 
 def prepare_image(image, target):
 	# if the image mode is not RGB, convert it
@@ -92,9 +92,43 @@ def predict():
 	# return the data dictionary as a JSON response
 	return render_template('response.html', data=data)
 
+@app.route("/predict_p", methods=["POST"])
+def predict_p():
+	# initialize the data dictionary that will be returned from the
+	# view
+	data = {"success": False}
+	print(flask.request.form)
+	# ensure an image was properly uploaded to our endpoint
+	if flask.request.method == "POST":
+
+		if flask.request.files.get("image"):
+			#response = requests.get(flask.request.form.get("image_url"))
+			#image = Image.open(io.BytesIO(response.content))
+			# read the image in PIL format
+			image = flask.request.files["image"].read()
+			image = Image.open(io.BytesIO(image))
+			# preprocess the image and prepare it for classification
+			image = prepare_image(image, target=(224, 224))
+
+			# classify the input image and then initialize the list
+			# of predictions to return to the client
+			preds = model.predict(image)
+			results = imagenet_utils.decode_predictions(preds)
+			data["predictions"] = []
+
+			# loop over the results and add them to the list of
+			# returned predictions
+			for (imagenetID, label, prob) in results[0]:
+				r = {"label": label, "probability": round(float(prob),2)}
+				data["predictions"].append(r)
+
+			# indicate that the request was a success
+			data["success"] = True
+			data["image"] = flask.request.files["image"]
+	# return the data dictionary as a JSON response
+	return render_template('response2.html', data=data)
 
 @app.route('/')
-@cache.cached(timeout=60)
 def index():
     return render_template("index.html")
 
@@ -106,7 +140,10 @@ def index():
 if __name__ == "__main__":
 	print(("* Loading Keras model and Flask starting server..."
 		"please wait until server has fully started"))
-	load_model()
+	#load_model()
+	global model
+	model = ResNet50(weights="imagenet")
+	print("Loaded")
 	app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key"))
